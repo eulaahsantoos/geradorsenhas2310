a1fc7540235cfcf9afae6d07d8b9addd72a0694e
MikeeeeVale <mikevale.etec.sp.gov.br@gmail.com>
2024-10-09 17:26:19 +0000

    aula0910 Toast

--- geradorSenhas/src/components/modal/index.js
+++ geradorSenhas/src/components/modal/index.js
@@ -1,12 +1,24 @@
-import {View, Text, StyleSheet, TouchableOpacity, Pressable} from "react-native";
+import { View, Text, StyleSheet, TouchableOpacity, Pressable } from "react-native";
+import * as Clipboard from 'expo-clipboard';
+import Toast from 'react-native-toast-message';
 
-export function ModalPassword({senha, handleClose}){
-    return(
+export function ModalPassword({ senha, handleClose }) {
+
+    function copyClipboard() {
+        Clipboard.setStringAsync(senha);
+        Toast.show({
+            type: 'success',
+            text1: 'Senha copiada',
+            text2: 'Senha copiada para a área de tranfêrencia!',
+        });
+    }
+
+    return (
         <View style={styles.container}>
             <View style={styles.content}>
                 <Text style={styles.title}>Senha gerada</Text>
 
-                <Pressable style={styles.innerPassword}>
+                <Pressable style={styles.innerPassword} onPress={copyClipboard}>
                     <Text style={styles.text}>
                         {senha}
                     </Text>
@@ -23,62 +35,64 @@ export function ModalPassword({senha, handleClose}){
                 </View>
 
             </View>
+
+            <Toast />
         </View>
     )
 }
 
 const styles = StyleSheet.create({
-    container:{
+    container: {
         backgroundColor: "rgba(24, 24, 24, 0.6)",
         flex: 1,
         alignItems: 'center',
-        JustifyContent: 'center',
+        justifyContent: 'center',
     },
-    content:{
+    content: {
         backgroundColor: "#fff",
         width: "85%",
         paddingTop: 24,
         paddingBottom: 24,
         alignItems: 'center',
-        JustifyContent: 'center',
+        justifyContent: 'center',
         borderRadius: 8,
-        },
-    title:{
+    },
+    title: {
         fontSize: 20,
         fontWeight: "bold",
         color: "#000",
         marginBottom: 24,
-        },
-    innerPassword:{
+    },
+    innerPassword: {
         backgroundColor: "#e0e0e0",
         width: "90%",
         padding: 14,
         borderRadius: 8,
-        },
-    text:{
-        color:"#FFF",
-        textAlign:"center",
     },
-    buttonArea:{
-        flexDirection:"row",
+    text: {
+        color: "#000",
+        textAlign: "center",
+    },
+    buttonArea: {
+        flexDirection: "row",
         width: "90%",
         marginTop: 8,
         alignItems: 'center',
-        JustifyContent: 'space-between',
+        justifyContent: 'space-between',
     },
-    button:{
+    button: {
         flex: 1,
         alignItems: 'center',
-        marginTop: 8,
+        marginTop: 14,
         marginBottom: 14,
         padding: 8,
     },
-    buttonSave:{
+    buttonSave: {
         backgroundColor: "#392DE9",
         borderRadius: 8,
     },
-    buttonSaveText:{
+    buttonSaveText: {
         color: '#FFF',
-        fontWeight:'bold',
+        fontWeight: 'bold',
     },
 })
